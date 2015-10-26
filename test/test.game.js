eeb0fa06e76589554e22938a16233c82d220c3dd
Phil Vollhardt <phil@suds.io>
2015-10-26 19:47:32 -0400

    fixed reference to game from champions

--- test/test.game.js
+++ test/test.game.js
@@ -13,14 +13,14 @@ lol.setConfig({
 describe('Games API',function(){
 	it('should get games for specified summoner(sexy hexy [19112268])',function(){
 		this.timeout(10000);
-		return lol.champion.getSummonerRecentGameHistoryById(19112268).should.eventually.be.an('Object');
+		return lol.game.getSummonerRecentGameHistoryById(19112268).should.eventually.be.an('Object');
 	});
 	it('should recent history of games for a specified summoner in Korea (cowsep[25531791])',function(){
 		this.timeout(10000);
-		return lol.champion.getSummonerRecentGameHistoryById(25531791,lol.region.KOREA).should.eventually.be.an('Object');
+		return lol.game.getSummonerRecentGameHistoryById(25531791,lol.REGION.KOREA).should.eventually.be.an('Object');
 	});
 	it('should fail to get a history of games for a specified invalid summoner id (-34])',function(){
 		this.timeout(10000);
-		return lol.champion.getSummonerRecentGameHistoryById(-34).should.eventually.be.rejected;
+		return lol.game.getSummonerRecentGameHistoryById(-34).should.eventually.be.rejected;
 	});
 });
